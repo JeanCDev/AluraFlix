4c4eaffc2975f814dc42da4a9b39542e91586bcb
Jean Carlos Gomes <jeangames15@gmail.com>
2020-07-28 16:41:26 -0300

    Aula 2 - final

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -7,7 +7,18 @@ function CadastroCategoria(){
       <PageDefault>
         <h1>Página de cadastro de Categoria</h1>
 
-
+        <form>
+          <label>
+            Nome da Categoria:
+            <input 
+              type="text" 
+            />
+          </label>
+          
+          <button>
+            Cadastrar
+          </button>
+        </form>
 
         <Link to='/'>
           Ir pra Home
